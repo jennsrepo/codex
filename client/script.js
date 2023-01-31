5c6a36c246a2c25123d5b11ed4ba89d25ef8180e
Jennifer Wu <jenniferwu@MacBook-Pro.attlocal.net>
2023-01-30 22:07:35 -0800

    fix .gitignore

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-9cji.onrender.com", {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify({
